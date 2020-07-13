e34b8709fc3c97ee8aa9e8324bf59eb2b80f4f4d
poojagit912 <36732363+poojagit912@users.noreply.github.com>
2020-07-13 22:42:11 +0530

    Update provider.tf

--- .gitignore/provider.tf
+++ .gitignore/provider.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.gcp_region_1
-  zone    = var.gcp_zone_1
+  project = var.projectId
+  region  = var.gcpRegion
+  zone    = var.gcpZone
 }
